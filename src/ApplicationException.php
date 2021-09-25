8afaf9d3ed0d9d8baf3ee624ba0c6a5fa9309592
taro-hida <sk8trou@gmail.com>
2021-09-25 16:33:00 +0900

    esa474 例外クラスを php-todo-app から移動

---
+++ src/ApplicationException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace ApplicationExceptions;
+
+use Exception;
+
+class ApplicationException extends Exception implements ApplicationExceptionInterface
+{
+
+}
